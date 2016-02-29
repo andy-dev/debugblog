44e7bcfaec67a8780981568f1e87f0783cab7dd2
Macedo <macedo@Ikigai.local>
2016-02-29 15:11:48 -0800

    initial yo angular setup

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name debugblogApp
+ * @description
+ * # debugblogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('debugblogApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name debugblogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the debugblogApp
+ */
+angular.module('debugblogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
