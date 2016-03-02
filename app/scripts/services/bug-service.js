59aa1c1a7ab29b6ee4073a5f35b6ee8596aa7a61
Macedo <macedo@Ikigai.local>
2016-03-02 10:22:06 -0800

    refactor bug-service to an IIFE

--- app/scripts/services/bug-service.js
+++ app/scripts/services/bug-service.js
@@ -1,36 +1,36 @@
-'use strict';
+(function(){
+  'use strict';
 
-/**
- * @ngdoc service
- * @name debugblog.BugService
- * @description
- * # BugService
- * Service in the debugBlog.
- */
-angular
-  .module('debugblogApp')
-  .service('BugService', function($http){
-    var service = {};
-    var API_URL = 'http://localhost:3000';
+  angular
+    .module('debugblogApp')
+    .service('BugService', BugService)
 
-    service.findAllBugs = function() {
-      return $http.get(API_URL.concat('/api/Bugs'));
+    BugService.$inject = ['$http'];
+
+    function BugService ($http){
+      var service = {};
+      var API_URL = 'http://localhost:3000';
+
+      service.findAllBugs = function() {
+        return $http.get(API_URL.concat('/api/Bugs'));
+      };
+
+      service.addBug = function(description){
+        return $http.post(API_URL.concat('/api/Bugs'), {
+          description: description,
+          isResolved: false
+        });
+      };
+
+      service.updateBug = function(bug){
+        return $http.put(API_URL.concat('/api/Bugs/').concat(bug.id), bug);
+      };
+
+      service.deleteBug = function(bugId){
+        return $http.delete(API_URL.concat('/api/Bugs/').concat(bugId));
+      };
+
+      return service;
     };
 
-     service.addBug = function(description) {
-      return $http.post(API_URL.concat('/api/Bugs'), {
-        description: description,
-        isResolved: false
-      });
-    };
-
-    service.updateBug = function(bug){
-      return $http.put(API_URL.concat('/api/Bugs/').concat(bug.id), bug);
-    }
-
-    service.deleteBug = function(bugId){
-      return $http.delete(API_URL.concat('/api/Bugs/').concat(bugId));
-    }
-
-    return service;
-  });
+})();
